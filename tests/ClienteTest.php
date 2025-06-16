8800aba1962cc8894483c0446271054b74cd9341
joselitoaaah <jose.fuentes10@virginiogomez.cl>
2025-06-16 11:47:56 -0400

    trabajo3 definitivo

---
+++ tests/ClienteTest.php
@@ -0,0 +1,25 @@
+<?php
+// tests/ClienteTest.php
+
+use PHPUnit\Framework\TestCase;
+
+class ClienteTest extends TestCase
+{
+    public function testNombreNoVacio()
+    {
+        $nombre = "José Fuentes";
+        $this->assertNotEmpty($nombre);
+    }
+
+    public function testEmailValido()
+    {
+        $email = "jose@example.com";
+        $this->assertMatchesRegularExpression("/^.+@.+\..+$/", $email);
+    }
+    
+    public function testTelefonoNumerico()
+    {
+        $telefono = "123456789";
+        $this->assertMatchesRegularExpression("/^[0-9]+$/", $telefono);
+    }
+}
